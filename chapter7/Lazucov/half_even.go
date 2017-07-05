3e6d1bf96eb8d47c59d9924bdc596aaeace100f2
palzuncoff <palzuncoff@yahoo.com>
2017-07-05 17:23:51 +0300

    Lazucov chapter7 HW 2 exercis add coment

--- chapter7/Lazucov/half_even.go
+++ chapter7/Lazucov/half_even.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// мне кажется что в задании ошибка "half(1) должна вернуть (0, false),
+// в то время как half(2) вернет (1, true)." 2/1 = 1 (нечет false)
+
 func half(x int) (int, bool) {
 	y := x / 2
 	if y%2 > 0 {
