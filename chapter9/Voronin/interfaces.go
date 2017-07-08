748a940462cc1e254ed99b8f0edaeec4c65e998f
nick <nicolai.voronin@gmail.com>
2017-07-08 11:58:07 +0300

    Voronin Chapter 9 Homework

---
+++ chapter9/Voronin/interfaces.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+type Circle struct {
+	x, y, r float64
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func (c *Circle) perimeter() float64 {
+	return math.Pi * 2 * c.r
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x2, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+func (r *Rectangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x2, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return (l + w) * 2
+}
+
+type Shape interface {
+	area() float64
+	perimeter() float64
+}
+
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	fmt.Println(c.area())
+	fmt.Println(c.perimeter())
+
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area())
+	fmt.Println(r.perimeter())
+
+	fmt.Println(totalArea(&c, &r))
+}
